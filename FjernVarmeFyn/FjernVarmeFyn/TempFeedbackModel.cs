ad7039c2ce10881476b363458a648b7943b31e4a
ToothbrushPenguin <38418425+ToothbrushPenguin@users.noreply.github.com>
2025-03-03 02:04:46 +0100

    send resive feedback ui missing binding

---
+++ FjernVarmeFyn/FjernVarmeFyn/TempFeedbackModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace FjernVarmeFyn
+{
+    public class TempFeedbackModel : INotifyPropertyChanged
+    {
+        private string _emne;
+        private string _ticket;
+        private string _system;
+        private string _status;
+        private string _adminPriority;
+        private string _priority;
+        private string _description;
+
+        public string Emne
+        {
+            get { return _emne; }
+            set
+            {
+                if (_emne != value)
+                {
+                    _emne = value;
+                    OnPropertyChanged(nameof(Emne));
+                }
+            }
+        }
+
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        public string Ticket
+        {
+            get { return _ticket; }
+            set
+            {
+                if (_ticket != value)
+                {
+                    _ticket = value;
+                    OnPropertyChanged(nameof(Ticket));
+                }
+            }
+        }
+
+        public string System
+        {
+            get { return _system; }
+            set
+            {
+                if (_system != value)
+                {
+                    _system = value;
+                    OnPropertyChanged(nameof(System));
+                }
+            }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        public string AdminPriority
+        {
+            get { return _adminPriority; }
+            set
+            {
+                if (_adminPriority != value)
+                {
+                    _adminPriority = value;
+                    OnPropertyChanged(nameof(AdminPriority));
+                }
+            }
+        }
+
+        public string Priority
+        {
+            get { return _priority; }
+            set
+            {
+                if (_priority != value)
+                {
+                    _priority = value;
+                    OnPropertyChanged(nameof(Priority));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- FjernVarmeFyn/FjernVarmeFyn/Views/UserSendFeedback.xaml.cs
+++ FjernVarmeFyn/FjernVarmeFyn/Views/UserSendFeedback.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Collections.ObjectModel;
+
 namespace FjernVarmeFyn.Views
 {
     /// <summary>
@@ -20,9 +22,49 @@ namespace FjernVarmeFyn.Views
     /// </summary>
     public partial class UserSendFeedback : Page
     {
+        public ObservableCollection<TempFeedbackModel> FeedbackItems { get; set; }
+
         public UserSendFeedback()
         {
             InitializeComponent();
+
+            // Initialize the collection with some sample data
+            // In a real application, this would come from a database or service
+            FeedbackItems = new ObservableCollection<TempFeedbackModel>
+            {
+                new TempFeedbackModel
+                {
+                    Emne = "Problem med login",
+                    Ticket = "T-1001",
+                    System = "Login",
+                    Status = "Åben",
+                    AdminPriority = "2",
+                    Priority = "5"
+                },
+                new TempFeedbackModel
+                {
+                    Emne = "Fejl i rapportmodul",
+                    Ticket = "T-1002",
+                    System = "Rapporter",
+                    Status = "Under behandling",
+                    AdminPriority = "1",
+                    Priority = "3"
+                },
+                new TempFeedbackModel
+                {
+                    Emne = "Forslag til ny funktion",
+                    Ticket = "T-1003",
+                    System = "Dashboard",
+                    Status = "Åben",
+                    AdminPriority = "3",
+                    Priority = "4"
+                }
+            };
+
+            // Set the DataContext to this page instance
+            this.DataContext = this;
         }
+
+
     }
 }
